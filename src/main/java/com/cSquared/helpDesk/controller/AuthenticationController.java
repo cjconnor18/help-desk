490005b1b8b7477a2a7d512f40473879e208e851
cjconnor18 <cjallen1988@gmail.com>
2020-04-26 08:51:37 -0500

    getMapped register.html

--- src/main/java/com/cSquared/helpDesk/controller/AuthenticationController.java
+++ src/main/java/com/cSquared/helpDesk/controller/AuthenticationController.java
@@ -2,8 +2,11 @@ package com.cSquared.helpDesk.controller;
 
 import com.cSquared.helpDesk.data.UserRepository;
 import com.cSquared.helpDesk.models.User;
+import com.cSquared.helpDesk.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
@@ -31,4 +34,14 @@ public class AuthenticationController {
     private static void setUserSessionKey(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
     }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        return "register";
+    }
+
+
+
+
 }
