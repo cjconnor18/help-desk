04b1c985978d1155d24d064f06ac413b4b11b395
cjconnor18 <cjallen1988@gmail.com>
2020-05-11 19:01:40 -0500

    created and defined SeverityLevel Enum

--- src/main/java/com/cSquared/helpDesk/models/SeverityLevel.java
+++ src/main/java/com/cSquared/helpDesk/models/SeverityLevel.java
@@ -1,4 +1,23 @@
 package com.cSquared.helpDesk.models;
 
 public enum SeverityLevel {
+
+    LOW ("Low"),
+    MEDIUM ("Medium"),
+    HIGH ("High"),
+    VERYHIGH("Very High");
+
+    private String displayName;
+
+    SeverityLevel(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
