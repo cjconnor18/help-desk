bb5ab21ff76130dde21d96a048c546fe787cf7da
cjconnor18 <cjallen1988@gmail.com>
2020-05-24 17:05:23 -0500

    show user lists and fixed nav links

--- src/main/java/com/cSquared/helpDesk/controller/UsersController.java
+++ src/main/java/com/cSquared/helpDesk/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.cSquared.helpDesk.controller;
 
 import com.cSquared.helpDesk.data.UserRepository;
 import com.cSquared.helpDesk.models.AccessLevel;
+import com.cSquared.helpDesk.models.Ticket;
 import com.cSquared.helpDesk.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("users")
@@ -39,21 +41,26 @@ public class UsersController {
     }
 
     @GetMapping("view/{userId}")
-    public String viewTicket (@PathVariable int userId,
-                              Errors errors,
+    public String viewUser(@PathVariable int userId,
                               HttpSession session,
                               Model model){
         User currentUser = authenticationController.getUserFromSession(session);
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) {
+            return "users/index";
+        }
+
+
         if(currentUser.getAccessLevel() == AccessLevel.USER) {
-            if(currentUser.getId() != userId) {
-                errors.rejectValue("userAccess", "user.accessLevel", "User access denied. Please contact your administrator.");
+            if(currentUser.getId() != user.get().getId()) {
+
                 return "users/index";
             } else {
                 model.addAttribute("user", currentUser);
             }
         } else {
 
-            model.addAttribute("user", userRepository.findById(userId));
+            model.addAttribute("user", user.get());
         }
 
 
